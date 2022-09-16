817d89f70ec362ba0c12bac16a9976d586fa35c3
axel <axelwa@outlook.com>
2022-09-16 01:57:04 -0500

    Test values of user

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include "Menu/menu.h"
+#include "CoreGame/game.h"
 
 
 
 int main(void)
 {
+    // Test variables 
+    int x=5, y=8; 
+    short gamemode=0;
+    // Menu needs functionality 
     menu();
+
+    //Board and game Init works fine with SxS given board sizes, 
+    //  preferably use s<100 as I left 3. spaces extra for big numbers on the table on the Y axis (Rows)
+    gameInit( &x,&y, &gamemode);
     return 0;
 }
 
