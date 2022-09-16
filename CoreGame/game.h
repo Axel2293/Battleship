b5598e2210dd5e1b5fd7689ac03733bfe32fb7c4
axel <axelwa@outlook.com>
2022-09-16 01:54:39 -0500

    Draw and Initialize board

---
+++ CoreGame/game.h
@@ -0,0 +1,27 @@
+
+typedef struct cell
+{
+    // Occupied or not
+    short cell_state;
+    // Link with a ship in the given cell
+    int ship_id;
+    // Cell recieved a hit or not
+    short hit; 
+}cell;
+
+typedef struct ship
+{
+    // Aircraft carrier (AC), Vessel (VS), Submarine(SM), Cruise (CR), Boat (BT)
+    char ship_type[3];
+    // Vertical or Hirizontal
+    char orientation;
+    // Sunk - not sunk
+    short sunk;
+}ship;
+
+
+void gameInit(int *userX, int *userY, short *game_mode);
+
+void boardInt(char (*board)[], int *x, int *y);
+
+void drawBoard(char (*board)[], int *x, int *y);
